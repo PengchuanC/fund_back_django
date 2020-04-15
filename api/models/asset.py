da01984e014cefdec39876169ce13ae83c21f5f7
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2020-04-15 14:45:33 +0800

    '修改资产配置表的模型'

--- api/models/asset.py
+++ api/models/asset.py
@@ -11,8 +11,8 @@ class Asset(models.Model):
     warrant = models.FloatField(default=0.0, verbose_name="权证")
     cash = models.FloatField(default=0.0, verbose_name="现金")
     other = models.FloatField(default=0.0, verbose_name="其他")
-    status = models.CharField(max_length=8, verbose_name="数据类型")
-    date = models.DateField(verbose_name="更新时间")
+    date = models.DateField(verbose_name="报告期")
+    update_date = models.DateField(verbose_name="更新时间")
 
     class Meta:
         db_table = "t_ff_asset"
